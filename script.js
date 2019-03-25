c51c0b8b9f81f9bb2655fd2f697a24209bbc0430
Piotr Trysiński <ptrysinski@gmail.com>
2019-03-25 12:37:40 +0100

    ex 1 to 4 done

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
-function avg(array){
-    // let sum = 0
-    // for (i=0; i<array.length; i++){
-    //     sum = sum + array[i]
-    // }
-    // return sum / array.length
+const sum = function (array){
+    let sum = 0
+    sum = array.reduce((x, y) => x+y)
+    return sum
+}
 
-    let sum = array.reduce((x, y) => x+y)
+const avg = function (array){
+    return sum(array) / array.length
+}
 
-    return sum / array.length
+const sortArray = function (array){
+    let sortedArray = array.sort(function(x,y){
+        return x-y;
+    });
+    return sortedArray
+}
+
+const median = function (array){
+
+    const sortedArray = sortArray(array)
+
+    if (sortedArray.length === 0) return 0;
+
+    var half = Math.floor(sortedArray.length / 2);
+
+    if (sortedArray.length % 2)
+        return sortedArray[half];
+    else
+        return (sortedArray[half - 1] + sortedArray[half]) / 2
+}
+
+const minValue = function (array){
+    const sortedArray = sortArray(array)
+    return sortedArray[0]
+}
+
+const maxValue = function (array){
+    const sortedArray = sortArray(array)
+    return sortedArray[sortedArray.length - 1]
 }
