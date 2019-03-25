763d1767710b920759430869a040f48c78660aca
Piotr Trysiński <ptrysinski@gmail.com>
2019-03-25 11:36:11 +0100

    ex 1 done

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 function avg(array){
-    let sum = 0
-    for (i=0; i<array.length; i++){
-        sum = sum + array[i]
-    }
+    // let sum = 0
+    // for (i=0; i<array.length; i++){
+    //     sum = sum + array[i]
+    // }
+    // return sum / array.length
+
+    let sum = array.reduce((x, y) => x+y)
+
     return sum / array.length
 }
